6ed0f6faf4c4e0888fd19c4b17636a7068001fbf
Shashank S <shashank5418shashu@gmail.com>
2025-02-10 23:46:03 +0530

    Using dotenv we used are using enviroment variable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-console.log("This is a final setup of production level backend project !!");
+import { app } from "./app.js";
+import  dotenv  from "dotenv";
 
+dotenv.config({
+    path:'./.env'
+}
+)
+
+
+const PORT = process.env.PORT || 8001
+
+app.listen(PORT , ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
 
