4ad0a5fd4a53e25810aac44885a3e7ab18293c90
Shashank S <shashank5418shashu@gmail.com>
2025-02-11 00:07:41 +0530

    Database name created and using try catch block to connect DB

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const mongooseConnection = async()=>{
+    try {
+        
+    } catch (error) {
+        console.log("There is an error in database connection" , error);
+    }
+}
+
+
