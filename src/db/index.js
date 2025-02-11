7d9f93e1ec30436c4466d4be6e55c415140df19f
Shashank S <shashank5418shashu@gmail.com>
2025-02-11 22:20:32 +0530

    DATABASE CONNECTED TO READ AND WRITE

--- src/db/index.js
+++ src/db/index.js
@@ -2,20 +2,23 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 
-const mongooseConnection = async()=>{
+const connectDB = async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.
             MONGODB_URI}/${DB_NAME}`)
 
         console.log(`Connection established Host Name ${connectionInstance.connection.host}`);
+
+        return connectionInstance;
         
 
 
     } catch (error) {
         console.log("There is an error in database connection" , error);
+        throw error
     }
 }
 
-export default mongooseConnection;
+export default connectDB;
 
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { app } from "./app.js";
 import  dotenv  from "dotenv";
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path:'./.env'
@@ -9,7 +10,14 @@ dotenv.config({
 
 const PORT = process.env.PORT || 8001
 
-app.listen(PORT , ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
 
+connectDB()
+.then(()=>{
+    app.listen(PORT , ()=>{
+        console.log(`Server is running on port ${PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDb connected failed",err);
+    
+})
