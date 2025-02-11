feb378fd668d14c16495335c6a59d4a7b5301596
Shashank S <shashank5418shashu@gmail.com>
2025-02-11 22:02:07 +0530

    Connected to Mongodb Atlas through env

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,21 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+
 const mongooseConnection = async()=>{
     try {
+        const connectionInstance = await mongoose.connect(`${process.env.
+            MONGODB_URI}/${DB_NAME}`)
+
+        console.log(`Connection established Host Name ${connectionInstance.connection.host}`);
         
+
+
     } catch (error) {
         console.log("There is an error in database connection" , error);
     }
 }
+
+export default mongooseConnection;
 
 
