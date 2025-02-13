afa0df5463bb55f91edd0e0428ea4c6debb89577
Shashank S <shashank5418shashu@gmail.com>
2025-02-14 00:15:54 +0530

    Generating Access Token

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -14,6 +14,7 @@
 
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema = new Schema(
   {
@@ -80,6 +81,13 @@ userSchema.pre("save", async function(next){
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password , this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign({
+        _id:this._id,
+        email:this.email
+    }, process.env.ACCESS_TOKEN_KEY , {expiresIn: process.env.ACCESS_EXPIRES_IN} )
 }
 
 export const Users = mongoose.model("Users", userSchema);
