cd0791c451527dc20ce20d7e9d032be542df3c97
Shashank S <shashank5418shashu@gmail.com>
2025-02-14 00:31:31 +0530

    Cookie Parser was added so that req will have cookie field extra now

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import healthCheckRouter from "./routes/heathcheckup.routes.js"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
@@ -13,7 +14,7 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true , limit:"16kb"}))
 app.use(express.static('public'))
-
+app.use(cookieParser())
 app.use("/api/v1/healthcheckRouter", healthCheckRouter)
 
 
