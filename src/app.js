f911fafc3a6d17f228c691666a527d2df96b6f31
Shashank S <shashank5418shashu@gmail.com>
2025-02-15 23:49:58 +0530

    Structure of user Route is completed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import healthCheckRouter from "./routes/heathcheckup.routes.js"
+import userRoute  from "./routes/RegisterUser.routes.js"
 import cookieParser from "cookie-parser"
 
 const app = express()
@@ -15,7 +16,10 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true , limit:"16kb"}))
 app.use(express.static('public'))
 app.use(cookieParser())
+
+
 app.use("/api/v1/healthcheckRouter", healthCheckRouter)
+app.use("/api/v1/users", userRoute)
 
 
 export {app}
--- src/routes/RegisterUser.routes.js
+++ src/routes/RegisterUser.routes.js
@@ -1,9 +1,16 @@
-import { Router} from "express";
+import { Router } from "express";
 
 import { RegisterUser } from "../controllers/RegisterUser.controllers.js";
+import { uploads } from "../middleware/multer.middleware.js";
 
-const router = Router()
+const router = Router();
 
-router.route("/Register").get(RegisterUser)
+router
+  .route("/register")
+  .post(uploads.fields([
+    { name: "avatar", maxCount: 1 }, 
+    {name:"coverImage", maxCount:1}
+]), 
+    RegisterUser);
 
-export default router
+export default router;
