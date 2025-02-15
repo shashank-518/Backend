6a4c1414409fcadf78f20d3d87982ffda68c5005
Shashank S <shashank5418shashu@gmail.com>
2025-02-15 09:13:38 +0530

    Writing a function on uploading to Cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,3 +1,20 @@
 import { v2 as cloudinary } from 'cloudinary';
-
+import fs from "fs"
 // config cloudinary setting
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDNAIRY_CLOUD_NAME, 
+    api_key: process.env.CLOUDNAIRY_API_KEY, 
+    api_secret: process.env.CLOUDNAIRY_SECRET_KEY
+});
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try {
+        
+    } catch (error) {
+        fs.unlink(localFilePath)
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
