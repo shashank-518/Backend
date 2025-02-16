1335c7197c9ab8dd9b45b686d96e826055a0f1b6
Shashank S <shashank5418shashu@gmail.com>
2025-02-16 23:43:40 +0530

    To Check Wheather the User Exists or not

--- src/controllers/RegisterUser.controllers.js
+++ src/controllers/RegisterUser.controllers.js
@@ -1,5 +1,6 @@
 import { ApiError } from "../utils/ApiError";
 import asyncHandler from "../utils/asyncHandler";
+import { Users } from "../models/users.models";
 
 const RegisterUser = asyncHandler(async(req,res)=>{
     const {fullname,username,password,email} = req.body;
@@ -7,6 +8,14 @@ const RegisterUser = asyncHandler(async(req,res)=>{
     if([fullname,username,password,email].some((field) => field?.trim() === "" )){
         throw new ApiError(404 , "There Was some Error")
     }
+
+    const existedUser = await Users.findOne({$or:[{username},{email}]})
+
+    if(existedUser){
+        throw new ApiError(409, "User Already Exists")
+    }
+
+    
 
 
 
