e79157f4342da5f64cf5591b74221fca0127fe2d
Shashank S <shashank5418shashu@gmail.com>
2025-02-16 20:10:38 +0530

    Building Register Controllers

--- src/controllers/RegisterUser.controllers.js
+++ src/controllers/RegisterUser.controllers.js
@@ -1,6 +1,15 @@
+import { ApiError } from "../utils/ApiError";
 import asyncHandler from "../utils/asyncHandler";
 
 const RegisterUser = asyncHandler(async(req,res)=>{
+    const {fullname,username,password,email} = req.body;
+
+    if([fullname,username,password,email].some((field) => field?.trim() === "" )){
+        throw new ApiError(404 , "There Was some Error")
+    }
+
+
+
 
 })
 
