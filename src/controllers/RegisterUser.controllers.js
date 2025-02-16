863f24213f478cc781f26c3bf4352404fb3b1efc
Shashank S <shashank5418shashu@gmail.com>
2025-02-16 23:58:58 +0530

    User is being added to database

--- src/controllers/RegisterUser.controllers.js
+++ src/controllers/RegisterUser.controllers.js
@@ -1,6 +1,7 @@
 import { ApiError } from "../utils/ApiError";
 import asyncHandler from "../utils/asyncHandler";
 import { Users } from "../models/users.models";
+import { uploadOnCloudinary } from "../utils/cloudinary";
 
 const RegisterUser = asyncHandler(async(req,res)=>{
     const {fullname,username,password,email} = req.body;
@@ -15,7 +16,27 @@ const RegisterUser = asyncHandler(async(req,res)=>{
         throw new ApiError(409, "User Already Exists")
     }
 
-    
+    const avatarlocalpath = req.files?.avatar[0]?.path
+    const coverlocalpath = req.files?.coverImage[0]?.path
+
+    if(!avatarlocalpath){
+        throw new ApiError(400 , "Avatar file is missing")
+    }
+
+    const avatarImage = uploadOnCloudinary(avatarlocalpath)
+
+    let coverImage = ""
+    if(coverlocalpath){
+        coverImage = uploadOnCloudinary(coverlocalpath) 
+    }
+
+    const user = await Users.create({
+        fullname,
+        username,
+        password,
+        avatar:avatarImage.url,
+        coverImage:coverImage?.url || ""
+    })
 
 
 
