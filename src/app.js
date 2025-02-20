f96186d82d24711c30cb629c3686ee604e7c0418
Shashank S <shashank5418shashu@gmail.com>
2025-02-20 15:52:58 +0530

    Changes made

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
     credentials:true
 }))
 
-// common middlewares
+// common middlewares that we will use
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true , limit:"16kb"}))
 app.use(express.static('public'))
