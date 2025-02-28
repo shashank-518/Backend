102338a05f7d018ee7c584500be5f0c41fb20034
Shashank S <shashank5418shashu@gmail.com>
2025-02-28 23:50:07 +0530

    isModified and minor changes made

--- src/controllers/RegisterUser.controllers.js
+++ src/controllers/RegisterUser.controllers.js
@@ -5,9 +5,9 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const RegisterUser = asyncHandler(async(req,res)=>{
-    const {fullname,username,password,email} = req.body;
+    const {fullName,username,password,email} = req.body;
 
-    if([fullname,username,password,email].some((field) => field?.trim() === "" )){
+    if([fullName,username,password,email].some((field) => field?.trim() === "" )){
         throw new ApiError(404 , "There Was some Error")
     }
 
@@ -51,7 +51,7 @@ const RegisterUser = asyncHandler(async(req,res)=>{
     }
 
     const user = await Users.create({
-        fullname,
+        fullName,
         username,
         password,
         avatar:avatarImage.url,
--- src/models/users.models.js
+++ src/models/users.models.js
@@ -72,8 +72,8 @@ const userSchema = new Schema(
 
 
 userSchema.pre("save", async function(next){
-
-    if(!this.modified("password")) return next()
+    // isModified is the method
+    if(!this.isModified("password")) return next()
 
     this.password = bcrypt.hash(this.password,10)
     next()
